92ac5bd696b1dc8270d2bd93d6c48f56a29ef988
Juanes46 <jegutierrezs@uniboyaca.edu.co>
2025-03-08 12:04:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<?php 
+echo " Hola mundo !!! ";     /* Generar nuestra estructura de datos*/
+echo '<br>';
+echo " Hola mundo !!! ";
+
+$nombre ='Pepe';
+$apellido = "Gomez";
+echo '<br> Hola ' . $nombre . ' ' . $apellido;
+echo "<br> Hola $nombre $apellido";
+$edad = 25;
+$estatura = 1.75;
+$mayorEdad = true; //false
+/**
+ * Bloque.....
+ */
+
+$prueba = null;
+$prueba = array(1,2,3,4,5,6);
+$prueba = [1,2,3,4,5,6];
+$prueba = [1,2.6,3,true,5,"a"];
+echo '<br>'. $prueba[2];
+
+$persona = [
+    "nombre" => "Pepe",
+    "apellido" =>"Gomez",
+    "edad" => 30,
+    "numerosSuerte" => [20,12,6],
+];
+echo '<br>' . $persona['nombre'];
